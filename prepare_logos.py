b462449e9a1879f6906cb9959f941220401c5dc4
Samuel Sergi <sam.sergi@outlook.com>
2021-12-25 17:43:06 +1100

    Modifying script to not fail if it doesn't find a logo.

--- prepare_logos.py
+++ prepare_logos.py
@@ -29,41 +29,44 @@ def main():
 
     # Loop through each server in index.
     for server in servers:
-        server_id = server['id']
-        server_name = server['name']
-        logo_source = '%s/%s.png' % (args.source, server_id)
-        logo_destination = '%s/%s.webp' % (args.destination, server_id)
+        try:
+            server_id = server['id']
+            server_name = server['name']
+            logo_source = '%s/%s.png' % (args.source, server_id)
+            logo_destination = '%s/%s.webp' % (args.destination, server_id)
 
-        # Check if server in index has an accompanying logo.
-        if not os.path.isfile(logo_source):
-            raise ValueError(
-                '%s does not have a server logo... Please ensure the file name matches the server ID and is a PNG.' %
-                    server_name
-            )
+            # Check if server in index has an accompanying logo.
+            if not os.path.isfile(logo_source):
+                raise ValueError(
+                    '%s does not have a server logo... Please ensure the file name matches the server ID and is a PNG.' %
+                        server_name
+                )
 
-        # Load image and check dimensions.
-        server_logo = Image.open(logo_source)
-        if server_logo.width != server_logo.height:
-            raise ValueError(
-                '%s\'s server logo does not have a 1:1 aspect ratio... Please ensure the image meets the requirements before proceeding.' %
-                    server_name
+            # Load image and check dimensions.
+            server_logo = Image.open(logo_source)
+            if server_logo.width != server_logo.height:
+                raise ValueError(
+                    '%s\'s server logo does not have a 1:1 aspect ratio... Please ensure the image meets the requirements before proceeding.' %
+                        server_name
+                )
+            if server_logo.width < 512:
+                raise ValueError(
+                        '%s\'s server logo width/height is less than 512px... Please ensure the image meets the requirements before proceeding.' %
+                            server_name
+                    )
+                
+            # Convert to Webp.
+            output = webptools.cwebp(
+                input_image=(logo_source),
+                output_image=(logo_destination),
+                option=('-lossless ' if args.lossless else '') + f'-resize {args.resize} {args.resize} -metadata none'
             )
-        if server_logo.width < 512:
-           raise ValueError(
-                '%s\'s server logo width/height is less than 512px... Please ensure the image meets the requirements before proceeding.' %
-                    server_name
-            )
-        
-        # Convert to Webp.
-        output = webptools.cwebp(
-            input_image=(logo_source),
-            output_image=(logo_destination),
-            option=('-lossless ' if args.lossless else '') + f'-resize {args.resize} {args.resize} -metadata none'
-        )
-        if output['exit_code']:
-            raise OSError(f'Failed to run cwebp on {logo_source}: {output}')
+            if output['exit_code']:
+                raise OSError(f'Failed to run cwebp on {logo_source}: {output}')
 
-        print('Successfully converted %s\'s logo.' % server_name)
+            print('Successfully converted %s\'s logo.' % server_name)
+        except Exception:
+            print('Couldn\'t process %s\'s logo... Skipping.' % server_name)
 
     print('Done!')
         
