a4eb8f77bfd5b9f557988c76342c9bcff687d2d5
Sam <sam.sergi@outlook.com>
2022-09-03 15:03:15 +1000

    Inactive List (#489)
    * Add inactive file + schema.
    
    * Bulk add inactive servers.

--- scripts/utils.py
+++ scripts/utils.py
@@ -4,6 +4,11 @@ import json
 def get_all_servers(servers_dir):
     servers = []
 
+    # Open inactive.json
+    inactive = []
+    with open(f"inactive.json") as inactive_file:
+        inactive = json.load(inactive_file)
+
     # Looping over each server folder
     for root, _dirs, _files in os.walk(servers_dir):
         server_id = root.split(os.path.sep)[-1]
@@ -14,6 +19,15 @@ def get_all_servers(servers_dir):
         with open(f"{servers_dir}/{server_id}/metadata.json") as server_file:
             server = json.load(server_file)
 
+        # Images
+        logo_path = f'{servers_dir}/{server_id}/logo.png'
+        background_path = f'{servers_dir}/{server_id}/background.png'
+
+        # Enrich server data
+        server["inactive"] = server["id"] in inactive
+        server["enriched"] = os.path.isfile(logo_path) and os.path.isfile(background_path)
+
+        # Add to list
         servers.append(server)
 
     # Sort A-Z
